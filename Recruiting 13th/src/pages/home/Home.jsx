c87f1871a1d1901232b610bc937f8c42aedada22
yeeun park <145003970+yengniws@users.noreply.github.com>
2025-02-09 16:16:28 +0900

    :hammer: Style: About 페이지 폴더 구조 생성

--- Recruiting 13th/src/pages/home/Home.jsx
+++ Recruiting 13th/src/pages/home/Home.jsx
@@ -1,5 +1,17 @@
+import Main from "./Main/Main";
+import About from "./About/About";
+import People from "./People/People";
+import Timeline from "./Timeline/Timeline";
+import * as S from "./HomeStyles";
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <S.HomeWrapper>
+      <Main />
+      <About />
+      <Timeline />
+      <People />
+    </S.HomeWrapper>
+  );
 };
 
 export default Home;
