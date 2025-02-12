005b5feab8f71905c5886a6930a9e045ebfc06dd
yeeun park <145003970+yengniws@users.noreply.github.com>
2025-02-12 14:41:36 +0900

    :lipstick: Feat: About 페이지 about 섹션 반응형 작업 완료

--- Recruiting 13th/src/pages/home/About/AboutStyles.js
+++ Recruiting 13th/src/pages/home/About/AboutStyles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import media from "../../../styles/Media";
 
 export const AboutContainer = styled.div `
   height: 100vh;
@@ -10,6 +11,16 @@ export const AboutContainer = styled.div `
   text-align: center;
   gap: 40px;
   margin-bottom: 300px;
+
+  ${media.tablet} {
+    margin-bottom: 100px;
+    gap: 20px;
+  }
+
+  ${media.mobile} {
+    margin-bottom: 0px;
+    gap: 10px;
+  }
 `;
 
 export const TitleContainer = styled.div `
@@ -23,17 +34,45 @@ export const TitleContainer = styled.div `
 export const Title = styled.div `
   font-size: 50px;
   font-weight: bolder;
+
+  ${media.tablet} {
+    font-size: 40px;
+  }
+
+  ${media.mobile} {
+    font-size: 25px;
+  }
 `;
 
 export const RowBar = styled.div `
   width: 280px;
   border-top: 1.5px solid black;
   margin-top: 20px;
+
+  ${media.tablet} {
+    width: 200px;
+    margin-top: 20px;
+  }
+
+  ${media.mobile} {
+    width: 130px;
+    margin-top: 15px;
+    border-top: 1px solid black;
+  }
 `;
 
 export const AboutTxtContainer = styled.div `
   font-size: 25px;
   line-height: 150%;
+
+  ${media.tablet} {
+    font-size: 20px;
+  }
+
+  ${media.mobile} {
+    font-size: 15px;
+    width: 280px;
+  }
 `;
 
 export const AboutBtnContainer = styled.div `
@@ -41,8 +80,17 @@ export const AboutBtnContainer = styled.div `
   justify-content: space-between;
   align-items: center;
   gap: 20px;
-  width: 35%;
+  width: 650px;
   margin-top: 50px;
+
+  ${media.tablet} {
+    width: 450px;
+  }
+
+  ${media.mobile} {
+    width: 270px;
+    gap: 15px;
+  }
 `;
 
 export const AboutBtn = styled.div `
@@ -58,10 +106,24 @@ export const AboutBtn = styled.div `
     isActive ? "#20be37" : "rgba(255, 119, 16, 0.1)"};
   color: ${({ isActive }) => (isActive ? "white" : "black")};
   transition: background-color 0.3s, color 0.3s;
+
+  ${media.tablet} {
+    font-size: 16px;
+    width: 100px;
+    height: 35px;
+    border-radius: 10px;
+  }
+
+  ${media.mobile} {
+    font-size: 10px;
+    width: 65px;
+    height: 23px;
+    border-radius: 8px;
+  }
 `;
 
 export const AboutContentContainer = styled.div `
-  width: 50%;
+  width: 930px;
   background-color: rgba(32, 190, 55, 0.05);
   border-radius: 20px;
   height: 300px;
@@ -69,6 +131,16 @@ export const AboutContentContainer = styled.div `
   gap: 5%;
   transition: background-color 0.3s ease-in-out, transform 0.3s ease-in-out;
   margin: 10px;
+
+  ${media.tablet} {
+    width: 650px;
+    height: 250px;
+  }
+
+  ${media.mobile} {
+    width: 330px;
+    height: 160px;
+  }
 `;
 
 export const AboutContentImg = styled.img `
@@ -77,6 +149,14 @@ export const AboutContentImg = styled.img `
   object-position: center left;
   border-radius: 20px 0px 0px 20px;
   transition: opacity 0.1s ease-in-out;
+
+  ${media.tablet} {
+    height: 250px;
+  }
+
+  ${media.mobile} {
+    height: 160px;
+  }
 `;
 export const AboutContentTxtWrapper = styled.div `
   display: flex;
@@ -85,13 +165,33 @@ export const AboutContentTxtWrapper = styled.div `
   align-items: self-start;
   width: 40%;
   gap: 20px;
+
+  ${media.mobile} {
+    gap: 10px;
+  }
 `;
 export const AboutContentTxtTitle = styled.div `
   font-size: 25px;
   font-weight: 1000;
+
+  ${media.tablet} {
+    font-size: 18px;
+  }
+
+  ${media.mobile} {
+    font-size: 12px;
+  }
 `;
 export const AboutContentTxtsection = styled.div `
   font-size: 20px;
   text-align: left;
   line-height: 200%;
+
+  ${media.tablet} {
+    font-size: 16px;
+  }
+
+  ${media.mobile} {
+    font-size: 10px;
+  }
 `;
